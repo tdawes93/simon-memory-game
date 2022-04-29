ff203882b11d5f5fec405f0ce196f76a65ca65e0
tdawes93 <tdawes136@googlemail.com>
2022-04-29 16:28:42 +0000

    Add data-listener code and test

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -53,6 +53,12 @@ describe("new game works correctly", () => {
     test("should display 0 for element id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
     })
+    test("expect data-listener to be true", () => {
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            expect(element.getAttribute("data-listener")).toBe("true");
+        }
+    });
 });
 
 describe("gameplay works correctly", () => {
