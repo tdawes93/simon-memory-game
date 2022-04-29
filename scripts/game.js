4a0c9bcee21761ac6a2a072ed65979e5652836e7
tdawes93 <tdawes136@googlemail.com>
2022-04-29 14:39:05 +0000

    Add game object and contents

---
+++ scripts/game.js
@@ -0,0 +1,10 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMove: [],
+    choices: ["button1", "button2", "button3", "button4"],
+};
+
+module.exports = {
+    game
+};
---
+++ scripts/tests/game.test.js
@@ -0,0 +1,34 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const {
+    game
+} = require("../game");
+
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("current game key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("player move key exists", () => {
+        expect("playerMove" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contains correct IDs", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
